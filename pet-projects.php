4ad4be4860044da5454b2e2d49817421c4c1608b
msconed <krossdar@mail.ru>
2024-09-02 13:31:34 +0300

    up

---
+++ pet-projects.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+/*
+
+
+
+Весьма простенький ToDo сервис:
+    https://github.com/msconed/todoshka
+
+
+В процессе создания Форума на Laravel, самая база почти готова.
+    https://github.com/msconed/Laravel_Site
+
+
+
+*/
+
+
+
+
